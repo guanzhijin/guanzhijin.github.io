bb77272a1f33779deb2bb8b3c7c40489737f6803
guanzhijin <1402888367@qq.com>
2025-06-11 16:18:10 +0800

    Update hearts.js

--- assets/js/hearts.js
+++ assets/js/hearts.js
@@ -1,3 +1,13 @@
+// 生成随机颜色的函数
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 document.addEventListener('click', function(e) {
     const heart = document.createElement('span');
     heart.className = 'heart';
