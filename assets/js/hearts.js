456d195848babcbb39e33f198b77b4568ee016ee
guanzhijin <1402888367@qq.com>
2025-06-11 16:18:31 +0800

    Update hearts.js

--- assets/js/hearts.js
+++ assets/js/hearts.js
@@ -8,11 +8,17 @@ function getRandomColor() {
     return color;
 }
 
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     const heart = document.createElement('span');
     heart.className = 'heart';
     heart.style.left = e.clientX + 'px';
     heart.style.top = e.clientY + 'px';
+    // 生成随机颜色并应用到爱心元素上
+    const randomColor = getRandomColor();
+    heart.style.backgroundColor = randomColor;
+    heart.querySelectorAll('::before, ::after').forEach((pseudoElement) => {
+        pseudoElement.style.backgroundColor = randomColor;
+    });
     document.body.appendChild(heart);
 
     setTimeout(() => {
